6402bd03c72fd86fb7c6f43463f2d599099a908c
Kirsiasc <kirsiasc@gmail.com>
2025-09-16 15:42:25 +0900

    Hunty_Zombie.lua

--- Hunty_Zombie.lua
+++ Hunty_Zombie.lua
@@ -143,7 +143,6 @@ local Toggle = Tab4:Toggle({
     Title = "AntiAFK",
     Desc = "Prevent Roblox from kicking you when idle",
     Icon = "shield",
-    Type = "Checkbox",
     Default = false,
     Callback = function(state)
         _G.AntiAFK = state
