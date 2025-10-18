5b087a4a02afb0750c1cbe343e71a2e84729c4d4
Kirsiasc <kirsiasc@gmail.com>
2025-10-18 17:37:35 +0900

    Test.lua

--- Fisch.lua
+++ Fisch.lua
@@ -1,1 +1,61 @@
+local success, WindUI = pcall(function()
+    return loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+end)
 
+if not success or not WindUI then
+    warn("⚠️ UI failed to load!")
+    return
+else
+    print("✓ UI loaded successfully!")
+end
+
+local Window = WindUI:CreateWindow({
+    Title = "Seraphin",
+    Icon = "rbxassetid://120248611602330",
+    Author = "KirsiaSC | Fisch",
+    Folder = "SERAPHIN_HUB",
+    Size = UDim2.fromOffset(270, 300),
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 170,
+    HasOutline = true
+})
+
+Window:Tag({
+    Title = "v0.0.0.1",
+    Color = Color3.fromHex("#9b4dff")
+})
+
+WindUI:Notify({
+    Title = "SeraphinHub Loaded",
+    Content = "UI loaded successfully!",
+    Duration = 3,
+    Icon = "bell",
+})
+
+local Tab1 = Window:Tab({
+    Title = "Home",
+    Icon = "house",
+})
+
+local Section = Tab1:Section({ 
+    Title = "Community Support",
+    TextXAlignment = "Left",
+    TextSize = 17,
+})
+
+Tab1:Button({
+    Title = "Discord",
+    Desc = "click to copy link",
+    Callback = function()
+        if setclipboard then
+            setclipboard("discord.gg/getseraphin")
+        end
+    end
+})
+
+local Section = Tab1:Section({ 
+    Title = "Every time there is a game update or someone reports something, I will fix it as soon as possible.",
+    TextXAlignment = "Left",
+    TextSize = 17,
+})
