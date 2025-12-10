e61e26fb24301fe2f6a294bcd5b5f1c113ffb3ec
Kirsiasc <kirsiasc@gmail.com>
2025-12-10 19:50:28 +0900

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,47 +1,32 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
-local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()successfully!")
-
-local Window = WindUI:CreateWindow({
-    Title = "Seraphin",
-    Icon = "rbxassetid://120248611602330",
-    Author = "",
-    Folder = "SERAPHIN_HUB",
-    Size = UDim2.fromOffset(280, 320),
-    Transparent = true,
-    Theme = "Dark",
-    SideBarWidth = 170,
-    HasOutline = true
-})
 
 local Window = Fluent:CreateWindow({
-    Title = "Seraphin"
+    Title = "Seraphin",
     SubTitle = "v0.0.0.1",
-    Icon = "rbxassetid://120248611602330",
     TabWidth = 160,
-    Size = UDim2.new(280, 320),
+    Size = UDim2.fromOffset(550, 350),
     Acrylic = true,
     Theme = "Darker",
     MinimizeKey = Enum.KeyCode.LeftControl
 })
 
 Fluent:Notify({
-        Title = "SeraphinHub Loaded",
-        Content = "Rivals script loaded!",
-        Duration = 3
+    Title = "SeraphinHub Loaded",
+    Content = "Rivals script loaded!",
+    Duration = 3
 })
 
 local Tabs = {
-    Info = Window:AddTab({ Title = "| Info", Icon = "Info" }),
-    Main = Window:AddTab({ Title = "| Main", Icon = "house" }),
+    Info = Window:AddTab({ Title = "| Info", Icon = "info" }),
+    Main = Window:AddTab({ Title = "| Main", Icon = "home" }),
     Settings = Window:AddTab({ Title = "| Settings", Icon = "settings" })
 }
 
-local Section = TabInfo:AddSection("Support Community")
+Tabs.Info:AddSection("Support Community")
 
-Info:Button({
+Tabs.Info:AddButton({
     Title = "Discord",
-    Desc = "Click to copy Discord link",
+    Description = "Click to copy Discord link",
     Callback = function()
         if setclipboard then
             setclipboard("https://discord.gg/getseraphin")
@@ -49,9 +34,9 @@ Info:Button({
     end
 })
 
-Info:AddParagraph({
-    Title = "Every time there is a game update or someone reports something, I will fix it as soon as possible."
+Tabs.Info:AddParagraph({
+    Title = "Update Information",
+    Content = "Every time there is a game update or someone reports something, I will fix it as soon as possible."
 })
 
-
-local Section = TabMain:AddSection("Automatic")
+Tabs.Main:AddSection("Automatic")
