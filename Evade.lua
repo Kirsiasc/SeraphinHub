ac56f3da0bdae69a48a5499627154891950c3d3e
Kirsiasc <kirsiasc@gmail.com>
2025-12-14 20:31:12 +0900

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -21,3 +21,40 @@ local Window = WindUI:CreateWindow({
     HasOutline = true
 })
 
+Window:Tag({
+    Title = "v0.0.0.4",
+    Color = Color3.fromRGB(180, 0, 255)
+})
+
+WindUI:Notify({
+    Title = "SeraphinHub Loaded",
+    Content = "Arsenal script loaded!",
+    Duration = 3,
+    Icon = "bell",
+})
+
+local Info = Window:Tab({ Title = "Info", Icon = "info" })
+
+Info:Section({
+    Title = "Community Support",
+    TextXAlignment = "Left",
+    TextSize = 17,
+})
+
+Info:Button({
+    Title = "Discord",
+    Desc = "Click to copy Discord link",
+    Callback = function()
+        if setclipboard then
+            setclipboard("https://discord.gg/getseraphin")
+        end
+    end
+})
+
+Info:Paragraph({
+    Title = "Support",
+    Desc = "Every time there is a game update or someone reports something, I will fix it as soon as possible."
+})
+
+local Main = Window:Tab({ Title = "Main", Icon = "House" })
+
