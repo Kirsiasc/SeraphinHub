ae9bb2aa4d2241865fa49640a1626d732ef2a438
Kirsiasc <kirsiasc@gmail.com>
2025-12-14 20:27:07 +0900

    Update Evade.lua

--- Evade.lua
+++ Evade.lua
@@ -1,1 +1,23 @@
-test
+local success, WindUI = pcall(function()
+    return loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+end)
+
+if not success or not WindUI then
+    warn("⚠️ UI failed to load!")
+    return
+else
+    print("✓ UI loaded successfully!")
+end
+
+local Window = WindUI:CreateWindow({
+    Title = "Seraphin",
+    Icon = "rbxassetid://120248611602330",
+    Author = "KirsiaSC | Evade",
+    Folder = "SERAPHIN_HUB",
+    Size = UDim2.fromOffset(280, 320),
+    Transparent = true,
+    Theme = "Dark",
+    SideBarWidth = 170,
+    HasOutline = true
+})
+
